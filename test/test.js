ecb5d2745b475b2d0c96f8af632022a62e7dad0d
Higor Cavalcanti <higordaniell@live.com>
2018-03-08 23:49:07 -0300

    Registrando, logando e deslogando

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
+//var request = require('superagent');
+const request = require('supertest');
+const describe = require('mocha').describe;
+const expect = require('chai').expect;
+const it = require('mocha').it;
 
-var URL = "http://demo.redmine.org/";
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
 
+const URL = "http://demo.redmine.org/";
 
-var expect = require('Chai').expect;
-var request = require('superagent');
+const URL_REGISTER = 'account/register';
+const URL_ACCOUNT = 'my/account';
+const URL_LOGOUT = 'logout';
 
 /*
 1.	   ENTRAR NO AMBIENTE http://demo.redmine.org/
@@ -12,15 +20,114 @@ var request = require('superagent');
 4.	   DESLOQUE O USUARIO NA OPÇÃO "SAIR"
 */
 describe('User signup', () => {
+		
+	const http = request(URL);
+	const agent = request.agent(URL);
+	
+	const user_name = Math.random().toString(36).substr(2, 10);
+	
+	var token;	
+	
 	it('should return 200', (done) => {
-		request.get(URL, (err, res, body) => {
-			expect(err).to.be.a('null');
+		http.get('/')
+		.end( (err, res) => {
+			expect( err ).to.be.a('null');
 			expect( res.statusCode ).to.equal(200);
 			done();
 		});
-	});
+	});	
 	
+	
+	it('should return a csrf-token', (done) => {
+		agent
+		.get( URL_REGISTER )
+		.end( (err, res) => {
+			expect( err ).to.be.a('null');
+			expect( res.statusCode ).to.equal(200);			
+			
+			var html = new JSDOM( res.text );
+			token = html.window.document.querySelector("input[name=authenticity_token]").value;
+			
+			expect( token ).to.be.a('string');			
+			done();
+		});		
+	});	
+	
+	
+	it('should register new user', (done) => {	
+		
+		var user = {
+			'authenticity_token': token,
+			'user[login]': user_name,
+			'user[password]': 'senha',
+			'user[password_confirmation]': 'senha',
+			'user[firstname]': 'Sr',
+			'user[lastname]': user_name,
+			'user[mail]': user_name + '@asdff.com',
+			'user[language]': 'pt-BR',
+		};
+		
+		agent.post( URL_REGISTER )
+		.set('Content-Type', 'application/x-www-form-urlencoded')
+		.send(user)
+		.end( (err, res) => {			
+			expect( err ).to.be.a('null');
+			expect( res.statusCode ).to.equal( 302 );
+			expect( res.headers.location ).to.equal( URL + URL_ACCOUNT);
+			done();
+		});		
+	});	
+	
+	it('should be logged in', (done) => {
+		agent.get( URL_ACCOUNT )
+		.end( (err, res) => {			
+			expect( err ).to.be.a('null');
+			expect( res.statusCode ).to.equal( 200 );
+			
+			var html = new JSDOM( res.text );
+			var name = html.window.document.querySelector(".user.active").text;
+			
+			expect(name).to.equal( user_name );
+			
+			done();
+		});		
+	});	
+	
+	
+	it('should return a csrf-token to logout', (done) => {
+		agent
+		.get( URL_LOGOUT )
+		.end( (err, res) => {
+			expect( err ).to.be.a('null');
+			expect( res.statusCode ).to.equal(200);			
+			
+			var html = new JSDOM( res.text );
+			token = html.window.document.querySelector("input[name=authenticity_token]").value;
+			
+			expect( token ).to.be.a('string');			
+			done();
+		});		
+	});	
+	
+	it('should logout', (done) => {	
+		agent
+		.post( URL_LOGOUT )
+		.set('Content-Type', 'application/x-www-form-urlencoded')
+		.send({
+			"authenticity_token": token
+		})
+		.end( (err, res) => {						
+			expect( err ).to.be.a('null');
+			expect( res.statusCode ).to.equal( 302 );
+			expect( res.headers.location ).to.equal( URL );
+			
+			done();
+		});		
+	});
 });
+
+
+
 
 
 /*
